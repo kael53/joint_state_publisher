d3e2456ec0254d26acfb9d5becb3fa7c826d2503
Muhammed Kerem Kahraman <keremkahraman53@gmail.com>
2025-06-03 17:16:44 +0300

    default values

--- src/dex3_controller.cpp
+++ src/dex3_controller.cpp
@@ -348,9 +348,9 @@ private:
           }
         }
         hand_cmd.motor_cmd[j].q = target;
-        hand_cmd.motor_cmd[j].dq = 0.0f; // Set dq to 0 for position control
-        hand_cmd.motor_cmd[j].kp = 10.0f; // Moderate kp
-        hand_cmd.motor_cmd[j].kd = 0.01f; // Very low kd
+        hand_cmd.motor_cmd[j].dq = 0.f;
+        hand_cmd.motor_cmd[j].kp = 1.5f;
+        hand_cmd.motor_cmd[j].kd = 0.1f;
         hand_cmd.motor_cmd[j].tau = 0.f;
       }
       RCLCPP_INFO(this->get_logger(), "Calibrating joint %s: moving to upper limit, others to lower", joint_name.c_str());
